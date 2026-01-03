ed2fd36ebc71e18a8bd9ede5ecaeb528cfb2f7f2
nk-129904 <nitinkumar.kumar129904@marwadiuniversity.ac.in>
2026-01-03 16:35:36 +0530

    Delete models directory

--- models/Counter.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const counterSchema = new mongoose.Schema({
-  year: Number,
-  count: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model("Counter", counterSchema);
